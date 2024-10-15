0976b794595126dea2e0e3c678b10b7396d7556d
Vakiyama <vakiyama@outlook.com>
2024-10-14 21:45:50 -0700

    feat: logo and light mode switching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import Link from 'next/link';
+import LightLogo from '../assets/images/logos/light-logo.svg';
+import DarkLogo from '../assets/images/logos/dark-logo.svg';
 
 const geistSans = localFont({
   src: '../fonts/GeistVF.woff',
@@ -18,6 +20,20 @@ export const metadata: Metadata = {
   title: 'Harmony Blog',
   description:
     'Harmony Blogging platform documenting development of the Harmony caretaking application.',
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: LightLogo.src,
+        href: LightLogo.src,
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: DarkLogo.src,
+        href: DarkLogo.src,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -32,7 +48,21 @@ export default function RootLayout({
       >
         <div className="flex flex-row justify-center w-full ">
           <nav className="flex justify-between text-lg lg:px-8 px-5 border-b border-b-[#919191] w-full py-7 mb-4">
-            <Link href="/">logo here.</Link>
+            <Link href="/">
+              <picture>
+                <source
+                  srcSet={LightLogo.src}
+                  media="(prefers-color-scheme: light)"
+                />
+
+                <source
+                  srcSet={DarkLogo.src}
+                  media="(prefers-color-scheme: dark)"
+                />
+
+                <img src={LightLogo.src} />
+              </picture>
+            </Link>
             <div className="flex gap-4">
               <Link href="/blogs">Blogs</Link>
               <Link href="/about">About</Link>
